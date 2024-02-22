96b5a3a79898e736f12c4289cde0fa49ddb55538
Chris Leech <chrisleech@monzo.com>
2024-02-22 15:27:39 +0000

    add interactive add prompt and add line breaks

--- zsh/zshaliases/git.zsh
+++ zsh/zshaliases/git.zsh
@@ -1,15 +1,25 @@
 # why use lot words when few do trick
 alias branch="git checkout -b"
+
 # amend previous commit
 alias amend="git commit --amend --no-edit"
+
 # Show all uncommitted TODOs and FIXMEs
 alias todos="c; git diff | grep TODO | grep + | grep -v ISAV- | sed 's/+//g' | sed 's/$TAB//g'; git diff | grep FIXME | grep + | grep -v ISAV- | sed 's/+//g' | sed 's/$TAB//g'"
+
 # Revert the last commit
 alias whoops="git reset --soft HEAD~1"
+
 # Checkout master and pull quietly
 alias gcmp="git checkout master && git pull -q"
+
 # Merge master into feature branch
 alias gfm="git fetch && git merge origin/master"
+
+# Add using interactive prompt
+alias gap='git add -p'
+
+# functions
 
 function branchchris {
   git fetch -q && git switch -c chrisleech-$1 origin/master
